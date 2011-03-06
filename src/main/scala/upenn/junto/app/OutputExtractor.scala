6d7107e51e5b36a83c088081a27049fd7a12491d
Jason Baldridge <jasonbaldridge@gmail.com>
2011-03-05 22:03:25 -0600

    Options working correctly.

--- src/main/scala/upenn/junto/app/OutputExtractor.scala
+++ src/main/scala/upenn/junto/app/OutputExtractor.scala
@@ -31,26 +31,23 @@ object OutputExtractor {
 
   val NodeRE = """([^_]+)_(.+)""".r
     
-  val parser = new ArgotParser(
-    "Junto Output Extractor",
-    preUsage=Some("Junto 1.1")
-  )
+  val parser = new ArgotParser("Junto Output Extractor", preUsage=Some("Junto 1.1"))
 
   import ArgotConverters._
 
-  val typeToExtract = 
-    parser.multiOption[String](List("t", "type"), "nodetype",
+  val typeToExtractOption = 
+    parser.option[String](List("t", "type"), "nodetype",
                                "The type of node to extract information for. E.g. if you have WORD_hello, then use -t WORD to get the distribution for WORD_hello and not FOO_hello.")
 
 
   val input = parser.parameter[String]("input",
-                                       "Input file to read. If not " +
-                                       "specified, use stdin.",
-                                       false).toString
+				       "Input file to read. If not " +
+				       "specified, use stdin.",
+				       false)
 
   val output = parser.parameter[String]("outputfile",
                                         "Output file to which to write.",
-                                        false).toString
+                                        false)
 
   def main(args: Array[String]) = {
     try {
@@ -60,12 +57,11 @@ object OutputExtractor {
       case e: ArgotUsageException => println(e.message)
     }
 
-    println("*** " + typeToExtract)
+    val typeToExtract = typeToExtractOption.value.get
 
-
-    val outputFile = new FileWriter(new File(output))
+    val outputFile = new FileWriter(new File(output.value.get))
     
-    for (line <- Source fromFile(new File(input)) getLines()) {
+    for (line <- Source fromFile(new File(input.value.get)) getLines()) {
       
       val Array(nodename, gold, injected, estimated, isTestNode, mrr) = line.split('\t')
       val NodeRE(nodetype,nodeval) = nodename
@@ -76,16 +72,20 @@ object OutputExtractor {
           (for (i <- List.range(0,estimatedList.length,2)) 
              yield Pair(estimatedList(i),estimatedList(i+1).toDouble)).unzip
         
-        val dummyIndex = tags.indexOf("__DUMMY__")
-        
+        var dummyIndex = tags.indexOf("__DUMMY__")
+	if (dummyIndex == -1)
+	  dummyIndex = tags.length
+
         val activeTags = tags.slice(0,dummyIndex)
         var activeProbs = probs.slice(0,dummyIndex)
+
         activeProbs = activeProbs.map{_/activeProbs.sum}
         
         outputFile.write(nodeval)
 
         for ((tag,prob) <- activeTags.zip(activeProbs))
           outputFile.write(" " + tag + " " + prob)
+
         outputFile.write("\n")
         
       }
