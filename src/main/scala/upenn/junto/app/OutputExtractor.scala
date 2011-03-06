bde3db615fab4c98dc9c6b429b1352de485f9bd9
Jason Baldridge <jasonbaldridge@gmail.com>
2011-03-06 00:02:36 -0600

    Additional options, e.g. dummy flag.

--- src/main/scala/upenn/junto/app/OutputExtractor.scala
+++ src/main/scala/upenn/junto/app/OutputExtractor.scala
@@ -21,7 +21,6 @@ import java.io._
 import org.clapper.argot._
 
 
-
 /** 
  * Read Junto output to get distributions for just words, and only
  * for labels that have higher probability than __DUMMY__.
@@ -30,62 +29,63 @@ object OutputExtractor {
 
   val NodeRE = """([^_]+)_(.+)""".r
     
+
+  // Set up the options parser
+  import ArgotConverters._
   val parser = new ArgotParser("Junto Output Extractor", preUsage=Some("Junto 1.1"))
 
-  import ArgotConverters._
-
   val typeToExtractOption = 
-    parser.option[String](List("t", "type"), "nodetype",
-                               "The type of node to extract information for. E.g. if you have WORD_hello, then use -t WORD to get the distribution for WORD_hello and not FOO_hello.")
+    parser.multiOption[String](List("t", "type"), "nodetype",
+                               "The type of node to extract information for. E.g. if you have WORD_hello, then use -t WORD to get the distribution for WORD_hello and not FOO_hello. If no type is given, then all node types are extracted.")
 
+  val cutAtDummyFlag = 
+    parser.flag[Boolean](List("d", "dummy"),
+                         "Truncate distributions at the __DUMMY__ label, and renormalize.")
+  
+  val input = parser.parameter[String]("input", "Input file to read", false)
+  val output = parser.parameter[String]("outputfile", "Output file to write to.", false)
 
-  val input = parser.parameter[String]("input",
-				       "Input file to read. If not " +
-				       "specified, use stdin.",
-				       false)
-
-  val output = parser.parameter[String]("outputfile",
-                                        "Output file to which to write.",
-                                        false)
-
+  /**
+   * Main method -- do the work. It might be good eventually to have the values for
+   * each node type dumped to a node-specific file. 
+   */
   def main(args: Array[String]) = {
-    try {
-      parser.parse(args)
-    }
-    catch {
-      case e: ArgotUsageException => println(e.message)
-    }
+    try { parser.parse(args) }
+    catch { case e: ArgotUsageException => println(e.message); exit(0) }
 
-    val typeToExtract = typeToExtractOption.value.get
+    val typesToExtract = typeToExtractOption.value toSet
+    val doAllNodeTypes = typesToExtract isEmpty
+
+    val cutAtDummy = cutAtDummyFlag.value getOrElse(false)
 
     val outputFile = new FileWriter(new File(output.value.get))
     
-    for (line <- Source fromFile(new File(input.value.get)) getLines()) {
+    for (line <- Source fromFile(new File(input.value.get)) getLines) {
       
-      val Array(nodename, gold, injected, estimated, isTestNode, mrr) = line.split('\t')
+      val Array(nodename, gold, injected, estimated, isTestNode, mrr) = line split('\t')
       val NodeRE(nodetype,nodeval) = nodename
       
-      if (nodetype == typeToExtract && estimated != "") {
-        val estimatedList = estimated.split(" ")
+      if ( doAllNodeTypes || (typesToExtract(nodetype) && estimated != "") ) {
+        val estimatedList = estimated split(" ")
         val (tags,probs) = 
           (for (i <- List.range(0,estimatedList.length,2)) 
-             yield Pair(estimatedList(i),estimatedList(i+1).toDouble)).unzip
+             yield Pair(estimatedList(i),estimatedList(i+1).toDouble)) unzip
         
-        var dummyIndex = tags.indexOf("__DUMMY__")
-	if (dummyIndex == -1)
-	  dummyIndex = tags.length
+        var dummyIndex = tags indexOf("__DUMMY__")
+	if ( !cutAtDummy || dummyIndex == -1 )
+	  dummyIndex = tags length
 
-        val activeTags = tags.slice(0,dummyIndex)
-        var activeProbs = probs.slice(0,dummyIndex)
+        val activeTags = tags slice(0,dummyIndex)
+        var activeProbs = probs slice(0,dummyIndex)
 
-        activeProbs = activeProbs.map{_/activeProbs.sum}
+        activeProbs = activeProbs map{_/activeProbs.sum}
         
-        outputFile.write(nodeval)
+        outputFile write(nodeval)
 
-        for ((tag,prob) <- activeTags.zip(activeProbs))
-          outputFile.write(" " + tag + " " + prob)
+        for ( (tag,prob) <- activeTags zip(activeProbs) )
+          outputFile write(" " + tag + " " + prob)
 
-        outputFile.write("\n")
+        outputFile write("\n")
         
       }
     }
